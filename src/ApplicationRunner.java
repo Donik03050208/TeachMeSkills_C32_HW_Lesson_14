82016a1d0ae46cb6fa6cba9a0b1ef34ab1a1468b
Donik03050208 <goncharov-bogdan@internet.ru>
2024-11-25 19:21:38 +0300

    Add true file path

--- src/ApplicationRunner.java
+++ src/ApplicationRunner.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String fileName = scanner.nextLine();
         DocumentService.processDocuments(fileName);
--- src/utils/Constant.java
+++ src/utils/Constant.java
@@ -5,9 +5,9 @@ public interface Constant {
     String CONTRACT_NAME = "contract";
     int DOCUMENT_LENGTH = 15;
     String VALID_DOC_REPORT_PATH =
-            "TeachMeSkills_C32_HW_Lesson_14";
+            "resources/report/invalid_docs_report.txt";
     String VALID_CONTRACT_REPORT_PATH =
-            "C:\\Users\\а\\IdeaProjects\\TeachMeSkills_C32_HW_Lesson_14\\resources\\report\\valid_contracts_report.txt";
+            "resources/report/valid_contracts_report.txt";
     String INVALID_DOC_REPORT_PATH =
-            "C:\\Users\\а\\IdeaProjects\\TeachMeSkills_C32_HW_Lesson_14\\resources\\report\\invalid_docs_report.txt";
+            "resources/report/valid_docs_report.txt";
 }
